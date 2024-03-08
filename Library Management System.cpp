c41658531e52dc9ab9b4e8c05543fa3c90bb34f6
meowuscat <118561006+Escavine@users.noreply.github.com>
2024-03-08 11:22:51 +0000

    adding all relevant functions

--- Library Management System.cpp
+++ Library Management System.cpp
@@ -5,7 +5,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <chrono>
+#include <thread>
 
 
 // Defines all users (i.e. students and librarians)
@@ -80,6 +81,8 @@ class librarian : user // Librarian inherits properties of a user
             if (usernameInput == username && passwordInput == password)
             {
                 std::cout << "\nIndividual authenticated." << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(3)); // Wait three seconds
+                system("CLS"); // After 3 seconds, recurse through the program to allow the individual to read the message
                 librarianDashboard();
                 
             }
@@ -97,13 +100,15 @@ class librarian : user // Librarian inherits properties of a user
 
         void librarianDashboard()
         {
-            int userChoice;
+            std::string userChoice;
 
             std::vector <std::string> dashboardOptions = { "Register a book", "Borrow a book", "Return a book", "Log out" }; // Vector of string options
             // int maxSize = dashboardOptions.max_size(); Determine the maximum value of the 
 
             int numCount = 1;
-            std::cout << "Stepwise University - Staff/Librarian Dashboard\n";
+            std::cout << "Stepwise University: Staff/Librarian Dashboard\n";
+            std::cout << "\n"; // Spacing
+            std::cout << "Welcome " << name << std::endl;
             std::cout << "\n"; // Spacing
 
             for (int i = 0; i < dashboardOptions.size(); i++) // Iterate through the vector values, displaying them to the staff member
@@ -114,38 +119,66 @@ class librarian : user // Librarian inherits properties of a user
 
             std::cout << "\n"; // Spacing
             std::cout << "Enter a corresponding value: " << std::endl;
-            std::cin >> userChoice;
+            std::getline(std::cin, userChoice);
 
             // Selection case as switch case will break the program since string isn't supported (average C# user)
 
-            if (userChoice == 1)
+            if (userChoice == "1")
             {
+                // Relevant parameters for book registering
+                std::string bookID;
+                std::string bookTitle;
+                int yearOfBook;
+                std::string bookPublisher;
+                int numberOfBooks;
+                int numberOfAvailableBooks;
+                std::vector <std::string> relevantSubjects = {}; // A vector that will contain all necessary subjects of the books registered
+                std::cout << "Stepwise University: Registering a book\n";
+
+                std::cout << "Enter the book ID: " << std::endl;
+                std::getline(std::cin, bookID);
+
+                std::cout << "Enter the book title: " << std::endl;
+                std::getline(std::cin, bookTitle);
+
+                std::cout << "Enter the year of book published: " 
+
+
+
 
 
             }
-            else if (userChoice == 2)
+            else if (userChoice == "2")
             {
 
 
 
             }
-            else if (userChoice == 3)
+            else if (userChoice == "3")
             {
 
 
 
             }
-            else if (userChoice == 4)
+            else if (userChoice == "4")
             {
                 // int remainingChances = 3;
                 // std::cout << "Logging user out of session." << std::endl;
                 // login(remainingChances);  
+            }
+            else
+            {
+                std::cout << "\nInvalid input, please try again." << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(3)); // Wait three seconds
+                system("CLS"); // After 3 seconds, recurse through the program to allow the individual to read the message
+                librarianDashboard(); // Recurse 
             }
 
         }
 
     protected:
         // Test data for authenticating login
+        std::string name = "Khalid";
         std::string username = "Eucladian";
         std::string password = "euclade1";
 
@@ -188,6 +221,9 @@ class student : user // Student inherits properties of a user
             if (usernameInput == username && passwordInput == password)
             {
                 std::cout << "\nStudent authenticated." << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(3)); // Wait three seconds
+                system("CLS"); // After 3 seconds, recurse through the program to allow the individual to read the message
+                studentDashboard(); // Lead user to their dashboard
             }
             else
             {
